6d1de9d30727e9bddf338bc060537baf4b51cebe
pkulik0 <me@pkulik.com>
2025-04-10 17:24:04 +0000

    Correct architecture key in yaml file

--- process.py
+++ process.py
@@ -59,7 +59,8 @@ def process_revision(snap_name: str, revision: int) -> Tuple[str, str]:
             snap_yaml = yaml.safe_load(f)
 
     version = snap_yaml["version"]
-    architecture = snap_yaml["architecture"][0]  # Each kernel has only one architecture
+    # Each revision supports only one architecture
+    architecture = snap_yaml["architectures"][0]
     logging.debug(f"Revision {revision} is version {version} for {architecture}")
     return version, architecture
 
